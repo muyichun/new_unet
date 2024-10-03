47b483b4c614ed988be1e6cc0d74bc2c1039267e
muyichun <289651938@qq.com>
2024-10-03 23:39:57 +0800

    fxxxx

--- UNet2/architect/train.py
+++ UNet2/architect/train.py
@@ -3,22 +3,25 @@ import os
 import tqdm
 from torch import optim
 from torch.utils.data import DataLoader
+
+from UNet3.architect.net import UNet
 from data import *
-from net import *
+from xxxxnet import *
 from torchvision.utils import save_image
 
 '''
     初始参数配置
 '''
-weight_path = r'/Users/muyichun/PycharmProjects/new_unet/UNet2/unet.pth'
+weight_name = "unet.pth"
+weight_path = r'/Users/muyichun/PycharmProjects/new_unet/UNet3/'
 train_img = 'exp_128'
 train_label = 'label_128'
 valid_img = 'exp_valid_128'
 valid_label = 'label_valid_128'
 data_path = r'/Users/muyichun/Desktop/Demo/MCF_speckle_digits/'
-save_path = r'/Users/muyichun/PycharmProjects/new_unet/UNet2/train_image/'
-batch_size = 2
-epochs = 20
+save_path = r'/Users/muyichun/PycharmProjects/new_unet/UNet3/train_image/'
+batch_size = 1
+epochs = 200
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -30,18 +33,18 @@ if __name__ == '__main__':
     train_data_loader = DataLoader(MyDataset(data_path, train_img, train_label), batch_size=batch_size, shuffle=True)
     valid_data_loader = DataLoader(MyDataset(data_path, valid_img, valid_label), batch_size=batch_size, shuffle=True)
     net = UNet().to(device) #GPU
-    if os.path.exists(weight_path):
-        net.load_state_dict(torch.load(weight_path))
+    if os.path.exists(weight_path + weight_name):
+        net.load_state_dict(torch.load(weight_path + weight_name))
         print('### successful load weight！')
     else:
         print('### not successful load weight')
 
     opt = optim.Adam(net.parameters(), lr=1e-4)
     loss_fun = nn.BCELoss()
-    # loss_fun = nn.CrossEntropyLoss()    多分类任务中
 
     best_score = 999
     for epoch in range(epochs):
+        # 训练
         net.train()
         for i, (image, label) in enumerate(tqdm.tqdm(train_data_loader)):
             image, label = image.to(device), label.to(device) #GPU
@@ -55,7 +58,7 @@ if __name__ == '__main__':
                 print(f'{epoch+1}-{i}-train_loss===>>{train_loss.item()}')
             img = torch.stack([image[0], label[0], out_image[0]], dim=0)
             save_image(img, f'{save_path}/{i}.png')
-
+        # 验证
         net.eval()
         with torch.no_grad():
             total_loss = 0.0
@@ -71,4 +74,4 @@ if __name__ == '__main__':
         # 保存性能最好的模型
         if best_score > average_loss:
             best_score = average_loss
-            torch.save(net.state_dict(), weight_path)
+            torch.save(net.state_dict(), weight_path + str(round(best_score,4)) + weight_name)
